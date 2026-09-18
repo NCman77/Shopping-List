71122cb0029c038e0b97519a51982594385f02ed
NCman77 <166708529+NCman77@users.noreply.github.com>
2026-09-18 19:13:24 +0800

    fix: delete removed item-card media from Drive

--- src/client/app/background-personalization.js
+++ src/client/app/background-personalization.js
@@ -156,6 +156,7 @@ export async function runBackgroundSaveTransaction({
     } else if (removeRequested) {
       nextPreferences = normalizePersonalization({
         ...nextPreferences,
+        backgroundFiles: [],
         backgroundFileId: '',
         backgroundFileName: '',
         backgroundMimeType: ''
